64aa2957558d23c86a41317f6240a8c67826de1f
tharris715 <timothyharris715@gmail.com>
2023-04-25 14:11:17 -0500

    completed prep 3-5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        int guess = 0;
+        
+        while (guess != number)
+        {
+            Console.WriteLine("Guess a number between 1 and 100: ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > number)
+            {
+                Console.WriteLine("Too high, try a lower number.");
+                //guess = int.Parse(Console.ReadLine());
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Too low, try a higher number.");
+                //guess = int.Parse(Console.ReadLine());
+            }
+            else
+            {
+                Console.WriteLine("You got it!");
+            }         
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,57 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int response = -1;
+
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (response != 0)
+        {
+            Console.Write("Enter number: ");
+            response = int.Parse(Console.ReadLine());
+            
+            if (response != 0)
+            {
+                numbers.Add(response);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        
+        float average = ((float)sum) / numbers.Count();
+        Console.WriteLine($"The average is: {average}");
+        
+        int min = 999999;
+        foreach (int number in numbers)
+        {
+            if (number > 0 && number < min)
+            {
+                min = number;
+            }
+        }
+        Console.WriteLine($"The lowest positive number is {min}");
+
+        int max = numbers.Max();
+        Console.WriteLine($"The largest number is: {max}");
+
+        Console.WriteLine("Here is the sorted list:");
+        numbers.Sort();
+        foreach (int number in numbers)
+        {
+            Console.WriteLine($"{number}");
+        }
+
+
     }
 }
