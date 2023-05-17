768b9d32ff0d513f55bc7b084063251c3c9d6ae2
tharris715 <timothyharris715@gmail.com>
2023-05-17 16:10:18 -0500

    Start on the scripture program

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        // This will clear the console
+        Console.Clear();
+
+
+        Scripture scrip = new Scripture();
+
+        scrip.Display();
+
+        // need to first display the scripture - the scripture heading and the actual words are different, the 
+
+        Console.WriteLine();
+        Console.WriteLine("Press Enter to continue, or type quit to finish.");
+        string choice = Console.ReadLine();
+        
+        while (choice == "") {
+            
+            // run code to start hiding words, finish when all words are hidden
+            scrip.Hide();
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to continue, or type quit to finish.");
+            choice = Console.ReadLine();
+            if (choice.ToLower() == "quit") {
+                break;
+            }
+        }
+        
+
     }
 }
---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,46 @@
+public class Scripture{
+
+    private string _scripture = "Adam fell that men might be and men are that they might have joy.";
+
+    private string _header = "2 Nephi 2:25";
+
+
+    // Set and get header probably don't need this for the header
+    public string GetHeader() {
+        return _header;
+    }
+    public void SetHeader(string head) {
+        _header = head;
+    }
+
+    // Set and get scripture
+    public string GetScripture() {
+        return _scripture;
+    }
+    public void SetScripture(string scripture) {
+        _scripture = scripture;
+    }
+
+    public void Display() {
+        Console.WriteLine($"{_header} {_scripture}");
+    }
+
+    public void Hide()
+{
+    string[] verse = _scripture.Split(' ');
+    Random random = new Random();
+
+    for (int i = 0; i < 3; i++)
+    {
+        int randomIndex = random.Next(verse.Length);
+        verse[randomIndex] = new string('_', verse[randomIndex].Length);
+
+    }
+
+    string replacedString = string.Join(" ", verse);
+    SetScripture(replacedString);
+    Console.Clear();
+    Display();
+}
+
+}
