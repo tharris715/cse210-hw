35387bb93fa64ea98a347b182312e5004b3f20d2
tharris715 <timothyharris715@gmail.com>
2023-06-01 08:31:54 -0500

    Completed Learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment HW1 = new Assignment("Tim Harris", "Inheritance");
+        
+        // testing assignment
+        Console.WriteLine(HW1.GetSummary());
+
+        // testing MathAssignment
+        MathAssignment math1 = new MathAssignment("Timothy Harris", "Fractions", "Section 7.3", "Problems 8-19");
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
+
+        // testing WritingAssignment
+        WritingAssignment essay = new WritingAssignment("Timm Harris", "World War 2","The Causes of World War II");
+        Console.WriteLine(essay.GetSummary());
+        Console.WriteLine(essay.GetWritingInformation());
+
     }
 }
