7405acfd8c8f6a5e6735743bfd3976e3cce87605
tharris715 <timothyharris715@gmail.com>
2023-06-13 09:24:21 -0500

    Completed learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // square
+        Square shape1 = new Square("red", 3);
+        Console.WriteLine($"The square is {shape1.GetColor()}, and the area is {shape1.GetArea()}");
+    
+        // rectangle
+        Rectangle shape2 = new Rectangle("blue", 3, 8);
+        Console.WriteLine($"The rectangle is {shape2.GetColor()}, and the area is {shape2.GetArea()}");
+    
+        // circle
+        Circle shape3 = new Circle("green", 5);
+        Console.WriteLine($"The circle is {shape3.GetColor()}, and the area is {shape3.GetArea()}");
+    
+        // list
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(shape1);
+        shapes.Add(shape2);
+        shapes.Add(shape3);
+
+        foreach (Shape shape in shapes) {
+            string col = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"The color is {col}, and the area is {area}");
+        }
     }
 }
---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,18 @@
+public abstract class Shape {
+
+    private string _color;
+
+    public string GetColor() {
+        return _color;
+    }
+    public void SetColor(string color) {
+        _color = color;
+    }
+
+    public Shape(string color) {
+        SetColor(color);
+    }
+
+    public abstract double GetArea();
+
+}
