4908478522919a433a06f50203374fa3b5ac70ad
tharris715 <timothyharris715@gmail.com>
2023-07-14 08:32:21 -0500

    Moved comment methods to comment class, completed

--- final/Foundation1/Comment.cs
+++ final/Foundation1/Comment.cs
@@ -8,5 +8,23 @@ public class Comment {
         _text = text;
     }
 
+    //method to count comments
+    public int CommentCount(List<Comment> comments) {
+        int count = 0;
+        foreach (Comment comment in comments) {
+            count += 1;
+        }
+        return count;
+    }
+    //method to display comments
+    public string DisplayComment(List<Comment> comments) {
+        string com = "";
+        int count = 1;
+        foreach (Comment comment in comments) {
+            com += $"\n{count}. {comment._name}: \"{comment._text}\" ";
+            count += 1;
+        }
+        return com;
+    }
 
 }
--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -13,28 +13,11 @@ public class Video {
         _seconds = seconds;
         _comments = comments;
     }
-    //method to count comments
-    public int CommentCount(List<Comment> comments) {
-        int count = 0;
-        foreach (Comment comment in comments) {
-            count += 1;
-        }
-        return count;
-    }
-    //method to display comments
-    public string DisplayComment(List<Comment> comments) {
-        string com = "";
-        int count = 1;
-        foreach (Comment comment in comments) {
-            com += $"\n{count}. {comment._name}: \"{comment._text}\" ";
-            count += 1;
-        }
-        return com;
-    }
+    
     //method to combine all video and comment information to display to console.
     public void DisplayVideo() {
-
-        Console.WriteLine($"\"{_title}\" created by {_author}. {_seconds} seconds long, # of comments: {CommentCount(_comments)} {DisplayComment(_comments)}");
+        Comment com = new Comment("","");
+        Console.WriteLine($"\"{_title}\" created by {_author}. {_seconds} seconds long, # of comments: {com.CommentCount(_comments)} {com.DisplayComment(_comments)}");
     }
 
 }
